575d156f07604e8f27b12993699a0c74a3093293
theminhdayma <147359915+theminhdayma@users.noreply.github.com>
2024-01-11 14:40:14 +0700

    Add files via upload
    Nguyễn Thế Minh

---
+++ session21/index.js
@@ -0,0 +1,32 @@
+// // hiểu ngầm có var demoHoisting;
+
+// console.log(demoHosting);
+// var demoHosting = "Đây là biến hoisting";
+// console.log(demoHosting);
+
+// let fname = "Thế Minh";
+// const age = 18;
+
+// demoHosting = "ở đây là ở đâu";
+// fname = "huhuhuhu";
+// age = 20;
+
+// let duongCoBeoKhong = true;
+// const anCoNghienKhong = false; 
+// var status = true;
+// const ugly = false;
+
+// console.log(2 == '2');
+
+// && ||
+
+
+if(condition1){
+    // logic-1
+}else if(condition2){
+    // logic-2
+}else if(condition3){
+    // logic-3
+}else{
+    // logic-4
+}
